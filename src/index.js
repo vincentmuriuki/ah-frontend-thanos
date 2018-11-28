535960d346f78dc7ec554f11366451f92f455dc0
Bruce Allan Makaaru <brucemakallan@MacBook-Pro-6.local>
2018-11-28 04:36:16 +0300

    chore(redux): Setup Redux with React
    - Install Redux and React-Redux
    - Connect NavBar component to redux for a demonstration
    - Add reducers, actions, containers, and components
    
    [#161348745]

--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
-import ReactDOM from 'react-dom';
-import React from 'react'
-import App from './components/App';
+import React from "react";
+import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
+import store from "./store";
+import App from "./components/App";
 
-
-ReactDOM.render(<App />, document.getElementById('app'));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
---
+++ src/reducers/articleReducer.js
@@ -0,0 +1,9 @@
+// handle all actions pertaining to the article (create, read, ...)
+const article = (state = {}, action) => {
+  switch (action.type) {
+    default:
+      return state;
+  }
+};
+
+export default article;
